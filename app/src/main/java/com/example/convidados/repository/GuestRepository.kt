1cf94184ae9715d6fbf4039927eec0fcee48a844
Felipe <lipeigansi@gmail.com>
2023-03-10 16:41:16 -0300

    Criacao da recyclerView

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -7,35 +7,36 @@ import com.example.convidados.model.GuestModel
 
 
 // Função: É quem lida com manipulação do banco de dados
-class GuestRepository  private constructor(context: Context){
+class GuestRepository private constructor(context: Context) {
 
     private val guestDataBase = GuestDataBase(context)
 
-    companion object{
+    companion object {
         /*** Singleton explicação
          * É uma forma de garantir que algo seja executado APENAS uma vez
          * Nesse caso na primeira vez que exevutar esse código, será setado a instancia de GuestRepository
          * Mas se for chamado mais uma vez apenas retornará o próprio repository
          *
          *
-            private lateinit var repository : GuestRepository
-            fun getInstance(): GuestRepository {
-                if (!::repository.isInitialized){
-                    repository = GuestRepository()
-                }
-                return repository
-            }
+        private lateinit var repository : GuestRepository
+        fun getInstance(): GuestRepository {
+        if (!::repository.isInitialized){
+        repository = GuestRepository()
+        }
+        return repository
+        }
          *
          * */
-        private lateinit var repository : GuestRepository
+        private lateinit var repository: GuestRepository
         fun getInstance(context: Context): GuestRepository {
-            if (!Companion::repository.isInitialized){
+            if (!Companion::repository.isInitialized) {
                 repository = GuestRepository(context)
             }
             return repository
         }
     }
-    fun insert(guest: GuestModel): Boolean{
+
+    fun insert(guest: GuestModel): Boolean {
         return try {
             val db = guestDataBase.writableDatabase
             val presence = if (guest.presenca) 1 else 0
@@ -46,9 +47,69 @@ class GuestRepository  private constructor(context: Context){
 
             db.insert("Guest", null, values)
             true
-        }catch (e:Exception){
+        } catch (e: Exception) {
             false
         }
     }
-    fun update(){}
+
+    fun update(guest: GuestModel) {
+        val db = guestDataBase.writableDatabase
+        val presence = if (guest.presenca) 1 else 0
+
+        val values = ContentValues()
+
+        values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+        values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+
+        val selection = DataBaseConstants.GUEST.COLUMNS.ID + ""
+
+        val args = arrayOf(guest.id.toString())
+
+        db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+
+    }
+
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + "= ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun getGuests(valueOfPresence: String) : List<GuestModel>{
+
+        val list = mutableListOf<GuestModel>()
+        try {
+            val db = guestDataBase.readableDatabase
+
+            val column = mapOf(
+                "id" to DataBaseConstants.GUEST.COLUMNS.ID,
+                "name" to DataBaseConstants.GUEST.COLUMNS.NAME,
+                "presence" to DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            val cursor = db.rawQuery("Select * from Guest where presence =$valueOfPresence", null)
+            if (cursor != null && cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val id = cursor.getInt(cursor.getColumnIndex(column["id"]))
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(column["name"]))
+                    val presence =
+                        cursor.getInt(cursor.getColumnIndex(column["presence"]))
+                    list.add(GuestModel(id, name, presence ==1))
+                }
+            }
+            cursor.close()
+        } catch (e: Exception ){
+            return list
+        }
+        return list
+    }
+
 }
--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -1,14 +1,19 @@
 package com.example.convidados.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.viewmodel.AllGuestsViewModel
 import com.example.convidados.databinding.FragmentAllGuestsBinding
+import com.example.convidados.view.adapter.GuestAdapter
 
 class AllGuestsFragment : Fragment() {
 
@@ -17,23 +22,29 @@ class AllGuestsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: AllGuestsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val viewModel =
-            ViewModelProvider(this).get(AllGuestsViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, bundle: Bundle?): View {
+        viewModel = ViewModelProvider(this)[AllGuestsViewModel::class.java]
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textAllGuests
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Layout RecyclerView
+        binding.recyclerAllGuests.layoutManager = LinearLayoutManager(context)
+
+        // Adapter
+        binding.recyclerAllGuests.adapter = GuestAdapter()
+
+        viewModel.getPresece()
+
+        observe()
+        return binding.root
+    }
+
+    private fun observe() {
+        viewModel.guests.observe(viewLifecycleOwner) {
+            val a = ""
+            //Lista de convidados
         }
-        return root
     }
 
     override fun onDestroyView() {
