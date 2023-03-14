a1cbfc28ffbafc8278a6a260b5bdf7fbd12fa9de
FelipeIgansi <lipeigansi@gmail.com>
2023-03-14 17:03:28 -0300

    Room - Depencias e criacao da camada BD

--- app/src/main/java/com/example/convidados/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/convidados/repository/GuestDataBase.kt
@@ -3,25 +3,39 @@ package com.example.convidados.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.convidados.constants.DataBaseConstants
+import com.example.convidados.model.GuestModel
 
 
 // Função conecxão com o banco de dados
-class GuestDataBase(context: Context?) : SQLiteOpenHelper(context, NAME, null, VERSION) {
-    companion object{
-        private const val NAME = "guestdb"
-        private const val VERSION = 1
-    }
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDataBase() : RoomDatabase() {
+    companion object {
+        // Singletop será transferido para cá
 
-    override fun onCreate(db: SQLiteDatabase?) {
-        // A entity já substitui esse script
-        db?.execSQL("create table ${DataBaseConstants.GUEST.TABLE_NAME}" +
-                "( ${DataBaseConstants.GUEST.COLUMNS.ID} integer primary key autoincrement, " +
-                "${DataBaseConstants.GUEST.COLUMNS.NAME} text, " +
-                "${DataBaseConstants.GUEST.COLUMNS.PRESENCE} integer)")
-    }
+        private lateinit var INSTANCE: GuestDataBase
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
+        fun getDataBase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isInitialized) {
+                synchronized(GuestDataBase::class) {
+                    INSTANCE =
+                        Room.databaseBuilder(context, GuestDataBase::class.java, "guestdb")
+                            .addMigrations(MIGRATION_1_2)// Se precisar alterar a base deve fazer uma migration
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
+        private val MIGRATION_1_2: Migration = object : Migration(1,2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DELETE FROM Guest")
+            }
+        }
     }
 }
